7114d35788db59e28674088c078f475f96a38572
DarthVader-24 <andriiperevoshchenko@gmail.com>
2025-03-11 12:04:01 +0200

    Init: Basic Angular app with user components and styling

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,47 @@
+//import { DUMMY_USERS } from './../dummy-users';
+import { Component, computed, Input, signal, input } from '@angular/core';
+
+// const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  // // selectedUser = DUMMY_USERS[randomIndex];
+
+  // //get imagePath() {
+  // //  return 'assets/users/' + this.selectedUser.avatar;
+  // //}
+
+  // // onSelectUser(){
+  // //   const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+  // //   this.selectedUser = DUMMY_USERS[randomIndex];
+  // // }
+
+  // selectedUser = signal(DUMMY_USERS[randomIndex]);
+
+  // imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar);
+
+  // onSelectUser() {
+  //   const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+  //   this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  // }
+
+  // @Input({required: true}) avatar!: string;
+  // @Input({required: true}) name!: string;
+
+  avatar = input.required<string>();
+  name = input.required<string>();
+  
+
+  get imagePath(){
+    return 'assets/users/' + this.avatar;
+  }
+
+  onSelectUser(){
+
+  }
+}
