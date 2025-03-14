448c183ea643d7fe48ee1cb1af47e61431fa05a8
DarthVader-24 <andriiperevoshchenko@gmail.com>
2025-03-14 17:23:45 +0200

    Feat: add shared card component to reuse the customized styles, create task service and separate the logic, omit unnecessary events handling, add localStorage saving for keeping data after reloading the page

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -11,6 +11,7 @@ import {
 } from '@angular/core';
 
 import { type User } from './user.model';
+import { CardComponent } from '../shared/card/card.component';
 
 // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
@@ -22,7 +23,7 @@ import { type User } from './user.model';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [CardComponent],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
